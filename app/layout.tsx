5ea93b1f8d55fe9569ea9d4cd6325d57bd651ab2
IIFR-DEV-TEAM <iifrdevteam@gmail.com>
2025-01-19 16:21:06 +0530

    Added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <SpeedInsights /></body>
     </html>
   )
 }
